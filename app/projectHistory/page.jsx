786ff713e401a1615f481c41f80ef24adf55c3d5
niagarafalls729 <wltn729@gmail.com>
2024-06-10 21:58:23 +0900

    프로젝트 변경사항 메뉴를 -> 다른 이름으로, PC 일때는 우측에 메뉴, 모바일에는 메인 메뉴 밑 하위메뉴로 진행하는중

--- app/projectHistory/page.jsx
+++ app/projectHistory/page.jsx
@@ -3,7 +3,8 @@
 import { useEffect, useState } from 'react';
 import GitHubCalendar from 'react-github-calendar';
 import axios from 'axios';
-
+import dayjs from 'dayjs';
+import styles from './projectHistory.module.css';
 export default function History() {
   const [commits, setCommits] = useState([]);
   const [isMounted, setIsMounted] = useState(false);
@@ -34,32 +35,37 @@ export default function History() {
     <>
       {isMounted && (
         <>
-          <p>
-            1,일단 여기다가 깃허브를 어떻게든 연동을 시켜서 커밋 기록을 손쉽게
-            볼 수 있도록한다.
-            <br />
-            2.날짜 api 자동으로 하루에 한번
-            <br />
-            3.DB 자동 닫힘을 방지해서 하루에 한번 select문이든 뭐든
-            의미없어보이는 행동 db로 연결
-            <br />
-            4.방문자 ip 수집
-            <br />
-            ... 위 사항은 모바일 환경 고려해서 반응형으로 개발
-            <br />
-          </p>
-          <GitHubCalendar
-            username={process.env.NEXT_PUBLIC_GITHUB}
-            showWeekdayLabels
-          />
-          <ul>
-            {commits.map(commit => (
-              <li key={commit.sha}>
-                <strong>{commit.commit.message}</strong>-{' '}
-                {commit.commit.author.date}
-              </li>
-            ))}
-          </ul>
+          <div className={styles['wrap']}>
+            <div className={styles['left']}></div>
+            <div className={styles['right']}>
+              <p>
+                1,일단 여기다가 깃허브를 어떻게든 연동을 시켜서 커밋 기록을
+                손쉽게 볼 수 있도록한다.
+                <br />
+                2.날짜 api 자동으로 하루에 한번
+                <br />
+                3.DB 자동 닫힘을 방지해서 하루에 한번 select문이든 뭐든
+                의미없어보이는 행동 db로 연결
+                <br />
+                4.방문자 ip 수집
+                <br />
+                ... 위 사항은 모바일 환경 고려해서 반응형으로 개발
+                <br />
+              </p>
+              <GitHubCalendar
+                username={process.env.NEXT_PUBLIC_GITHUB}
+                showWeekdayLabels
+              />
+              <ul>
+                {commits.map(e => (
+                  <li key={e.sha}>
+                    <strong>{e.commit.message}</strong>-{' '}
+                    {dayjs(e.commit.author.date).format('YYYY-MM-DD')}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </>
       )}
     </>
