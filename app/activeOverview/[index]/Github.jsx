cd23bacb172c902a9d5b696b27341b9ca63f107d
niagarafalls729 <wltn729@gmail.com>
2024-06-17 23:41:47 +0900

    node git까지 한번에 보이도록추가.

--- app/activeOverview/[index]/Github.jsx
+++ app/activeOverview/[index]/Github.jsx
@@ -5,31 +5,65 @@ import GitHubCalendar from 'react-github-calendar';
 import axios from 'axios';
 import dayjs from 'dayjs';
 export default function Github() {
-  const [commits, setCommits] = useState([]);
+  const [isReact, setIsReact] = useState([]);
+  const [isNode, setIsNode] = useState([]);
+  const [combinedCommits, setCombinedCommits] = useState([]);
   const [isMounted, setIsMounted] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     setIsMounted(true);
-    fetchCommits();
   }, []);
 
-  const fetchCommits = async () => {
-    const owner = process.env.NEXT_PUBLIC_GITHUB;
-    const repo = 'myminiblog_next';
-    const url = `https://api.github.com/repos/${owner}/${repo}/commits`;
-    const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
-    try {
+  const owner = process.env.NEXT_PUBLIC_GITHUB;
+  const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
+  const fetchAllCommits = async repo => {
+    let page = 1;
+    const perPage = 100;
+    let allCommits = [];
+
+    while (true) {
+      const url = `https://api.github.com/repos/${owner}/${repo}/commits?per_page=${perPage}&page=${page}`;
       const response = await axios.get(url, {
         headers: {
           Authorization: `token ${token}`,
         },
       });
-      setCommits(response.data);
-    } catch (error) {
-      setError(error.message);
+
+      if (response.data.length === 0) {
+        break;
+      }
+      allCommits = allCommits.concat(response.data);
+      page += 1;
     }
+
+    return allCommits;
   };
+  useEffect(() => {
+    const reactRepo = 'myminiblog_next';
+    const reactNode = 'node_api_blog';
+
+    const fetchCommits = async () => {
+      try {
+        const [reactCommits, nodeCommits] = await Promise.all([
+          fetchAllCommits(reactRepo),
+          fetchAllCommits(reactNode),
+        ]);
+
+        const combined = [...reactCommits, ...nodeCommits];
+        combined.sort(
+          (a, b) =>
+            new Date(b.commit.author.date) - new Date(a.commit.author.date)
+        );
+        setCombinedCommits(combined);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchCommits();
+  }, []);
+
   return (
     <>
       {isMounted && (
@@ -40,8 +74,9 @@ export default function Github() {
               username={process.env.NEXT_PUBLIC_GITHUB}
               showWeekdayLabels
             />
+
             <ul>
-              {commits.map(e => (
+              {combinedCommits.map(e => (
                 <li key={e.sha}>
                   <strong>{e.commit.message}</strong>-{' '}
                   {dayjs(e.commit.author.date).format('YYYY-MM-DD')}
