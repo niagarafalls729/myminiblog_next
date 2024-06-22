e1678fb71541a75817523126feb08632b02d4607
niagarafalls729 <wltn729@gmail.com>
2024-06-22 21:41:37 +0900

    테이블로 변경

--- app/activeOverview/[index]/Github.jsx
+++ app/activeOverview/[index]/Github.jsx
@@ -4,6 +4,13 @@ import { useEffect, useState } from 'react';
 import GitHubCalendar from 'react-github-calendar';
 import axios from 'axios';
 import dayjs from 'dayjs';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 export default function Github() {
   const [isReact, setIsReact] = useState([]);
   const [isNode, setIsNode] = useState([]);
@@ -35,11 +42,15 @@ export default function Github() {
       }
       const commitsWithRepoName = [];
       for (let e of response.data) {
-        e.repoName = repo;
+        if (repo == 'myminiblog_next') {
+          e.repoName = 'server';
+        } else {
+          e.repoName = 'front';
+        }
+
         commitsWithRepoName.push(e);
       }
 
-      console.log(commitsWithRepoName);
       allCommits = allCommits.concat(commitsWithRepoName);
       page += 1;
     }
@@ -81,16 +92,40 @@ export default function Github() {
               username={process.env.NEXT_PUBLIC_GITHUB}
               showWeekdayLabels
             />
-
+            {/* 
             <ul>
               {combinedCommits.map(e => (
                 <li key={e.sha}>
-                  <span>{e.repoName}</span>
-                  <strong>{e.commit.message}</strong>-{' '}
-                  {dayjs(e.commit.author.date).format('YYYY-MM-DD')}
+                  <span></span>
+                  <strong></strong>-{' '}
+                
                 </li>
               ))}
-            </ul>
+            </ul> 
+            */}
+
+            <TableContainer sx={{ maxWidth: 1050 }} component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>repository</TableCell>
+                    <TableCell>commit coment</TableCell>
+                    <TableCell align="right">date</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {combinedCommits.map(e => (
+                    <TableRow key={e.sha}>
+                      <TableCell sx={{ width: 100 }}>{e.repoName}</TableCell>
+                      <TableCell>{e.commit.message}</TableCell>
+                      <TableCell sx={{ width: 120 }} align="right">
+                        {dayjs(e.commit.author.date).format('YYYY-MM-DD')}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </div>
         </>
       )}
